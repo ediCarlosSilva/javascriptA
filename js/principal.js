e2094532c9f43e9e212db662ef00ac5a7fe20498
edi carlos Lubuntu <edi_csilva@hotmail.com>
2017-12-07 23:33:11 -0200

    inicio c4

--- js/principal.js
+++ js/principal.js
@@ -1,35 +1,51 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = 'Aparecida Nutricionista';
 
-var paciente = document.querySelector('#primeiro-paciente');
+// var paciente = document.querySelector('#primeiro-paciente');
+var pacientes = document.querySelectorAll('.paciente');
 
-var tdPeso = paciente.querySelector('.info-peso');
-var peso = tdPeso.textContent;
+// for (var i = 0; i < pacientes.length; i++) {
+//     var paciente = pacientes[i];
+//     console.log(paciente);
+// }
 
-var tdAltura = paciente.querySelector('.info-altura');
-var altura = tdAltura.textContent;
+for (var i = 0; i < pacientes.length; i++) {
 
-var tdImc = paciente.querySelector('.info-imc');
+    var paciente = pacientes[i];
 
-var pesoEhValido = true;
-var alturaEhValida = true;
-
-if (peso <= 0 || peso >= 400) {
-    pesoEhValido = false;
-    console.log('Peso Inválido!');
-    tdImc.textContent = 'Peso Inválido';
+    var tdPeso = paciente.querySelector('.info-peso');
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector('.info-altura');
+    var altura = tdAltura.textContent;
+    
+    var tdImc = paciente.querySelector('.info-imc');
+    
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+    
+    if (peso <= 0 || peso >= 400) {
+        pesoEhValido = false;
+        // console.log('Peso Inválido!');
+        tdImc.textContent = 'Peso Inválido';
+        paciente.classList.add('paciente-invalido');
+    }
+    
+    if (altura <= 0 || altura >= 3.00) {
+        alturaEhValida = false;
+        // console.log('Altura inválida!');
+        tdImc.textContent = 'Altura inválida';
+        paciente.classList.add('paciente-invalido');
+    }
+    
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
 
-if (altura <= 0 || altura >= 3.00) {
-    alturaEhValida = false;
-    console.log('Altura inválida!');
-    tdImc.textContent = 'Altura inválida';
-}
 
-if (alturaEhValida && pesoEhValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
+
 
 
 
